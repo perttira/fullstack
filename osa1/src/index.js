342300f4404fcbb92b34ed849defdc7ebcfc7be0
Pertti Rautio <perttira@gmail.com>
2018-10-22 15:26:15 +0300

    osa1 1.12* anekdootit osa1

--- osa1/src/index.js
+++ osa1/src/index.js
@@ -701,4 +701,57 @@ ReactDOM.render(
   document.getElementById('root')
 )
 
-*/
+*/   
+
+/*       1.12 ANEKDOOTIT OSA 1      */
+
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+       selected: 0
+
+    }
+  }
+
+
+  arvoSatunnaisluku = () => () => this.setState({selected: Math.floor((Math.random() * 6) + 0)})
+
+
+  render() {
+    return (
+      <div>
+        <div>
+          {this.props.anecdotes[this.state.selected]}
+        </div>
+        <div>
+          <Button
+            handleClick={this.arvoSatunnaisluku()}
+            text="next anecdote"
+          />
+        </div>
+      </div>
+    )
+  }
+}
+
+const anecdotes = [
+  'If it hurts, do it more often',
+  'Adding manpower to a late software project makes it later!',
+  'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+  'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+  'Premature optimization is the root of all evil.',
+  'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'
+]
+
+const Button = ({ handleClick, text}) => (
+  <button onClick={handleClick}>
+  {console.log(handleClick)}
+    {text}
+  </button>
+)
+
+ReactDOM.render(
+  <App anecdotes={anecdotes} />,
+  document.getElementById('root')
+)
