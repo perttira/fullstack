23a4012376445fde4a760d3de428486ca67a58a3
Pertti Rautio <perttira@gmail.com>
2019-01-15 14:58:49 +0200

    osa2 2.13* valmis

--- osa2_loput/src/App.js
+++ osa2_loput/src/App.js
@@ -10,11 +10,13 @@ class App extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
-      persons: [],
-      newNote: '',
+      countries: [],
       filter: '',
+      newNote: '',
       showAll: true
     }
+    this.handleClick = this.handleClick.bind(this);
+
     console.log('constructor')
   }
 
@@ -24,25 +26,22 @@ class App extends React.Component {
       .get('https://restcountries.eu/rest/v2/all')
       .then(response => {
         console.log('promise fulfilled')
-        this.setState({ persons: response.data })
+        this.setState({ countries: response.data })
       })
   }
 
   filtteroi = () => {
 
-   console.log("PITUUS", this.state.persons.filter(person => person.name.startsWith(this.state.filter)).length)
+   console.log("PITUUS", this.state.countries.filter(maa => maa.name.startsWith(this.state.filter)).length)
       
-      if(this.state.persons.filter(person => person.name.startsWith(this.state.filter)).length > 10 ) {
-        console.log("PITUUS IFFISSÄ", this.state.persons.filter(person => person.name.startsWith(this.state.filter)).length)
+      if(this.state.countries.filter(maa => maa.name.startsWith(this.state.filter)).length > 10 ) {
+        console.log("PITUUS IFFISSÄ", this.state.countries.filter(maa => maa.name.startsWith(this.state.filter)).length)
 
       return []
 
     } else {
-      return this.state.persons.filter(person => person.name.startsWith(this.state.filter))
+      return this.state.countries.filter(maa => maa.name.startsWith(this.state.filter))
     } 
-
-    
-       
   }
 
   handleChanges = (event) => {
@@ -59,44 +58,57 @@ class App extends React.Component {
     }
   }
 
+  handleClick(maa){
+      this.setState({ filter: maa })
+  }
+
   render() {
 
-    console.log("persons taulukko kokonaisuudessaan", this.state.persons)
+    console.log("countries taulukko kokonaisuudessaan", this.state.countries)
 
-   var montakoMaata = 't'
-   var notesToShow = this.filtteroi().map(person => <Henkilot key={person.id} name={person.name}/>)
+   var montakoMaata = ''
+   var notesToShow = this.filtteroi().map(maa => <Maa key={maa.id} name={maa.name} onClick={() => this.handleClick(maa.name)}/>)
    console.log("PITUUS RENDERISSÄ", this.filtteroi().length)
     if(this.filtteroi().length === 0){
       montakoMaata = "too many matches, specify another filter"
     }
     if(this.filtteroi().length === 1) {
-      notesToShow = this.filtteroi().map(person => <Henkilot luokka="naytaKaikkiTiedot" key={person.id} name={<h1>{person.name}</h1>} capital={<p>capital: {person.capital}</p>} population={<p>population: {person.population}</p>} flag={<img src={person.flag} alt="flag" height="100" width="200"/>}/>)
+      notesToShow = this.filtteroi().map(maa => <Maa  onClick={() => this.handleClick(maa.name)} luokka="naytaKaikkiTiedot" key={maa.id} name={<h1>{maa.name}</h1>} capital={<p>capital: {maa.capital}</p>} population={<p>population: {maa.population}</p>} flag={<img src={maa.flag} alt="flag" height="100" width="200"/>}/>)
       montakoMaata = ''
     }
     console.log('render')
 
     console.log("filtteroi()", this.filtteroi())
+    console.log("notesToShow", notesToShow)
+
+
     return (
 
       <div className="App">
         <label>find countries :</label>
-          <Rajaa name="filter" value={this.state.filter} onChange={this.handleChanges} persons={this.state.persons}/>
+          <Rajaa name="filter" onChange={this.handleChanges} value={this.state.filter} countries={this.state.countries}/>
           {montakoMaata}
         <ul>
-          {notesToShow}
+          {notesToShow}          
         </ul>
       </div>
     );
   }
 }
 
-const Henkilot = ({ luokka, name, capital, population, flag }) => {
+const Maa = (props) => {
+  console.log("onClick", props.onClick)
+
   return (
-    <li class={luokka}> {name}  {capital} {population} {flag} </li>
+
+    <ul class="maa">
+      <li  class={props.luokka}> <div value={props.name} onClick={props.onClick}> {props.name} </div> {props.capital} {props.population} {props.flag} </li>
+    </ul>
   )
 }
 
 const Rajaa = (props) => {
+
   return (
     <div>
       <input name={props.name} value={props.value}  onChange={props.onChange}/>
