2dcb69f79d238ee334a4c961bd7dcd8f8c947a57
Pertti Rautio <perttira@gmail.com>
2019-04-26 12:11:32 +0300

    4.18: blogilistan laajennus, step7

--- osa4/app.js
+++ osa4/app.js
@@ -9,6 +9,8 @@ const middleware = require('./utils/middleware')
 const morgan = require('morgan')
 const logger = require('./utils/logger')
 const usersRouter = require('./controllers/users')
+const loginRouter = require('./controllers/login')
+
 
 
 logger.info('connecting to', config.MONGODB_URI)
@@ -26,6 +28,7 @@ app.use(bodyParser.json())
 app.use(middleware.requestLogger)
 app.use('/api/blogs', blogsRouter)
 app.use('/api/users', usersRouter)
+app.use('/api/login', loginRouter)
 app.use(middleware.unknownEndpoint)
 app.use(middleware.errorHandler)
 
