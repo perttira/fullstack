0780eefa7413f8f1986e6fe3cba947eb0a5d50db
Pertti Rautio <perttira@gmail.com>
2019-05-06 18:08:31 +0300

    5.1: blogilistan frontend, step1

--- osa5/src/App.js
+++ osa5/src/App.js
@@ -3,22 +3,76 @@ import Blog from './components/Blog'
 import blogService from './services/blogs'
 
 const App = () => {
-  const [blogs, setBlogs] = useState([])
+  const [blogs, setBlogs] = useState([]) 
+  const [newNote, setNewNote] = useState('')
+  const [showAll, setShowAll] = useState(true)
+  const [errorMessage, setErrorMessage] = useState(null)
+  const [username, setUsername] = useState('') 
+  const [password, setPassword] = useState('') 
+  const [user, setUser] = useState(null)
 
   useEffect(() => {
-    blogService.getAll().then(blogs =>
-      setBlogs( blogs )
-    )  
+    blogService
+      .getAll().then(initialBlogs => {
+        setBlogs(initialBlogs)
+      })
   }, [])
+
+  // ...
+
+  const handleLogin = (event) => {
+    event.preventDefault()
+    console.log('logging in with', username, password)
+    setUser(true)
+  }
+  if (user === null) {
 
   return (
     <div>
-      <h2>blogs</h2>
-      {blogs.map(blog =>
-        <Blog key={blog.id} blog={blog} />
-      )}
+      <h1>Muistiinpanot</h1>
+
+      {/*<Notification message={errorMessage} />*/}
+
+      <h2>Kirjaudu</h2>
+
+      <form onSubmit={handleLogin}>
+        <div>
+          käyttäjätunnus
+            <input
+            type="text"
+            value={username}
+            name="Username"
+            onChange={({ target }) => setUsername(target.value)}
+          />
+        </div>
+        <div>
+          salasana
+            <input
+            type="password"
+            value={password}
+            name="Password"
+            onChange={({ target }) => setPassword(target.value)}
+          />
+        </div>
+        <button type="submit">kirjaudu</button>
+      </form>
+
+      // ...
     </div>
   )
+}else{
+  return(   
+     <div>
+      <h2>blogs</h2>
+      <p>{username} logged in</p>
+      {blogs.map(blog =>
+      <Blog key={blog.id} blog={blog} />
+    )
+    }
+  </div>)
 }
+}
+
+
 
 export default App
