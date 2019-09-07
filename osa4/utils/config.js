13d63e817f3e5c1c5cc831d3f7ae89669941fbf2
Pertti Rautio <perttira@gmail.com>
2019-09-07 11:01:37 +0300

    Blog.js GUI:n viilailua

--- osa4/utils/config.js
+++ osa4/utils/config.js
@@ -1,11 +1,13 @@
 /* Ympäristömuuttujien käsittely on eriytetty moduulin utils/config.js vastuulle */
 
-if (process.env.NODE_ENV !== 'production') {
+if (process.env.NODE_ENV === 'production') {
   require('dotenv').config()
 }
 
 let PORT = process.env.PORT
 let MONGODB_URI = process.env.MONGODB_URI
+
+console.log('process.env.MONGODB_URI', process.env.MONGODB_URI)
 
 if (process.env.NODE_ENV === 'test') {
   MONGODB_URI = process.env.TEST_MONGODB_URI
